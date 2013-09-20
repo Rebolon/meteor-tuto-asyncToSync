0c144676dd9be8be8aedbaa4cd67fd5ff5657f8c
Benjamin RICHARD <richard.tribes@gmail.com>
2013-09-20 12:11:10 +0000

    cleaner way to use the wrapper

--- meteor-tuto-asyncToSync.js
+++ meteor-tuto-asyncToSync.js
@@ -1,24 +1,34 @@
 if (Meteor.isServer) {
   Meteor.startup(function () {
+    //
+    var iconv = Meteor.require('iconv-lite'),
+        stdRequest = Npm.require('request');
+    var requestAsync = function(url, options, cb) {
+      if (typeof options.encoding === 'object') {
+        encoding = options.encoding;
+        options.encoding = null;
+      }
+      
+      stdRequest.get(url, options, function (err, response, body) {
+        if (encoding !== undefined) {
+          var buf = iconv.decode(response.body, encoding.src),
+              body = iconv.encode(buf, encoding.dest).toString('binary');
+          response.body = body;
+        }
+        
+        cb && cb(null, response);
+      });
+    },
+        requestSync = Meteor._wrapAsync(requestAsync);
+    
     // code to run on server at startup
-	console.log('startup');
-	var url = "http://www.leboncoin.fr/ventes_immobilieres/offres/rhone_alpes/rhone/?f=a&th=1&ps=8&pe=9&ros=5&roe=5&ret=1&location=Anse%2069480",
-	    iconv = Meteor.require('iconv-lite'),
-	    request = Npm.require('request'),
-	    fs = Npm.require('fs'),
-	    callMeLaterAsync = function(url, options, cb) {
-		request.get(url, options, function (err, response, body) {
-			cb && cb(null, response);
-		});
-	    },
-	    callMeLaterSync = Meteor._wrapAsync(callMeLaterAsync);
-
-	response = callMeLaterSync(url, {"encoding": null});
-	var buf = iconv.decode(response.body, 'iso-8859-15'),
-            body = iconv.encode(buf, 'iso-8859-1').toString('binary');
-
-	fs.writeFile('/home/action/workspace/projects/meteor-tuto-asyncToSync/dump.html', body);
-
-	console.log('end');
+    console.log('startup');
+    var url = "http://www.leboncoin.fr/ventes_immobilieres/offres/rhone_alpes/rhone/?f=a&th=1&ps=8&pe=9&ros=5&roe=5&ret=1&location=Anse%2069480",
+        fs = Npm.require('fs');
+    
+    response = requestSync(url, {"encoding": {"dest": "iso-8859-15", "src": "iso-8859-1"}});
+    fs.writeFile('/home/action/workspace/projects/meteor-tuto-asyncToSync/public/dump.html', response.body);
+    
+    console.log('end');
   });
 }
